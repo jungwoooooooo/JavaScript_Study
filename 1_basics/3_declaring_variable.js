5501693e660003322e21fcbdb6c28519a082e5b7
jungwoo <wjddnwh2@naver.com>
2024-10-02 15:14:58 +0900

    commit

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,21 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더 이상 안 씀
+ * 2) let
+ * 3) const
+ */
+var name = '조정우';
+console.log(name);
+
+var age = 25;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후에 변결 할 수 있음.
+ * 꽤 좋은 기능처럼 보이지만 나중에 보면 별로 좋지 않은 기능이다. ㅋ
+ */
